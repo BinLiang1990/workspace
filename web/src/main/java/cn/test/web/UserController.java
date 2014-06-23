6837c55c5a381c1eb75899472559f88b32cb5504
randy <randy1900@gmail.com>
2014-06-23 22:47:25 +0800

    save

--- web/src/main/java/cn/test/web/UserController.java
+++ web/src/main/java/cn/test/web/UserController.java
@@ -1,8 +1,10 @@
 package cn.test.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.test.domain.User;
@@ -11,9 +13,7 @@ import cn.test.domain.User;
 @RequestMapping("/user")
 public class UserController {
 
-	// @Resource
-	// private UserService userService;
-	// @ModelAttribute("user")
+	// 请求参数绑定到pojo
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView createUser(User user) {
 		// userService.createUser(user);
@@ -26,5 +26,21 @@ public class UserController {
 	@RequestMapping("/register")
 	public String register() {
 		return "user/register";
+	}
+
+	// 占位符,请求参数 http://127.0.0.1:8080/web/user/handle?wanglin
+	@RequestMapping(value = "/{vari}", params = "wanglin")
+	public String handle1(@PathVariable("vari") String username) {
+		System.out.println(username);
+		return "main/success";
+	}
+
+	// 请求参数绑定到方法入参
+	// http://127.0.0.1:8080/web/user/handle2?username=wanglin&password=123
+	@RequestMapping("/handle2")
+	public String handle2(@RequestParam("username") String username,
+			@RequestParam("password") String password) {
+		System.out.println(username + password);
+		return "main/success";
 	}
 }
